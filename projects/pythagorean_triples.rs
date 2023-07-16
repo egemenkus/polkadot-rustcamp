168ff9d82774547d5b07c7c26e9c08f89896e2fb
Egemen Kuş <47275560+egemenkus@users.noreply.github.com>
2023-07-16 17:31:16 +0300

    Update pythagorean_triples.rs

--- projects/pythagorean_triples.rs
+++ projects/pythagorean_triples.rs
@@ -4,14 +4,3 @@ use polkadot_rustcamp::pythagorean_triples;
 fn main() {
     pythagorean_triples::check(Some((3, 4, 5)));
 }
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = pythagorean_triples::check(Some((3, 4, 5)));
-        assert_eq!(result, true);
-    }
-}
